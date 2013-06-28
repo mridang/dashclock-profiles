d4be8cb20797e673829d2c6b8635007138b7e2a1
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2013-06-28 13:01:47 +0300

    Minor fixes

--- src/com/mridang/profilic/ProfilicWidget.java
+++ src/com/mridang/profilic/ProfilicWidget.java
@@ -4,6 +4,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.util.Log;
 
@@ -17,105 +18,108 @@ import com.mridang.profilic.R;
  */
 public class ProfilicWidget extends DashClockExtension {
 
-    /*
-     * @see com.google.android.apps.dashclock.api.DashClockExtension#onCreate()
-     */
-    public void onCreate() {
+	/*
+	 * @see com.google.android.apps.dashclock.api.DashClockExtension#onCreate()
+	 */
+	public void onCreate() {
 
-        super.onCreate();
-        Log.d("ProfilicWidget", "Created");
-        BugSenseHandler.initAndStartSession(this, "4a2e84b0");
+		super.onCreate();
+		Log.d("ProfilicWidget", "Created");
+		BugSenseHandler.initAndStartSession(this, "4a2e84b0");
 
-    }
+	}
 
-    /*
-     * @see
-     * com.google.android.apps.dashclock.api.DashClockExtension#onUpdateData
-     * (int)
-     */
-    @Override
-    protected void onUpdateData(int arg0) {
+	/*
+	 * @see
+	 * com.google.android.apps.dashclock.api.DashClockExtension#onUpdateData
+	 * (int)
+	 */
+	@Override
+	protected void onUpdateData(int arg0) {
 
-        setUpdateWhenScreenOn(true);
+		setUpdateWhenScreenOn(true);
 
-        Log.d("ProfilicWidget", "Getting the currently activated profile");
-        ExtensionData edtInformation = new ExtensionData();
-        edtInformation.visible(false);
+		Log.d("ProfilicWidget", "Getting the currently activated profile");
+		ExtensionData edtInformation = new ExtensionData();
+		edtInformation.visible(false);
 
-    Object o = getSystemService("profile");
-    try {
+		Object o = getSystemService("profile");
+		try {
 
-      Class<?> ProfileManager = Class.forName("android.app.ProfileManager");
-      Class<?> Profile = Class.forName("android.app.Profile");
-      try {
+			Class<?> ProfileManager = Class.forName("android.app.ProfileManager");
+			Class<?> Profile = Class.forName("android.app.Profile");
+			try {
 
-        Method getActiveProfile = ProfileManager.getDeclaredMethod("getActiveProfile", null);
-        Method getName = Profile.getDeclaredMethod("getName", null);
-        
-        try {
-          
-          switch (((AudioManager)getSystemService(Context.AUDIO_SERVICE)).getRingerMode()) {
-          case AudioManager.RINGER_MODE_SILENT:
-            edtInformation.visible(true);
-            edtInformation
-                .status(getString(R.string.current_profile)
-                    + " \u2014 " + (String) getName
-                        .invoke(getActiveProfile
-                            .invoke(o)));
-            edtInformation.expandedBody(getString(R.string.ringer_silent));
-            break;
+				Method getActiveProfile = ProfileManager.getDeclaredMethod("getActiveProfile", null);
+				Method getName = Profile.getDeclaredMethod("getName", null);
 
-          case AudioManager.RINGER_MODE_VIBRATE:
-            edtInformation.visible(true);
-            edtInformation
-                .status(getString(R.string.current_profile)
-                    + " \u2014 " + (String) getName
-                        .invoke(getActiveProfile
-                            .invoke(o)));
-            edtInformation.expandedBody(getString(R.string.ringer_vibrate));
-            break;
+				try {
 
-          case AudioManager.RINGER_MODE_NORMAL:
-            edtInformation.visible(true);
-            edtInformation
-                .status(getString(R.string.current_profile)
-                    + " \u2014 " + (String) getName
-                        .invoke(getActiveProfile
-                            .invoke(o)));
-            edtInformation.expandedBody(getString(R.string.ringer_normal));
-            break;
-          }
+					switch (((AudioManager)getSystemService(Context.AUDIO_SERVICE)).getRingerMode()) {
+					case AudioManager.RINGER_MODE_SILENT:
+						edtInformation.visible(true);
+						edtInformation
+						.status(getString(R.string.current_profile)
+								+ " \u2014 " + (String) getName
+								.invoke(getActiveProfile
+										.invoke(o)));
+						edtInformation.expandedBody(getString(R.string.ringer_silent));
+						edtInformation.clickIntent(new Intent("android.intent.action.PROFILE_PICKER"));
+						break;
 
-        } catch (IllegalAccessException e) {
-          e.printStackTrace();
-        } catch (InvocationTargetException e) {
-          e.printStackTrace();
-        }
+					case AudioManager.RINGER_MODE_VIBRATE:
+						edtInformation.visible(true);
+						edtInformation
+						.status(getString(R.string.current_profile)
+								+ " \u2014 " + (String) getName
+								.invoke(getActiveProfile
+										.invoke(o)));
+						edtInformation.expandedBody(getString(R.string.ringer_vibrate));
+						edtInformation.clickIntent(new Intent("android.intent.action.PROFILE_PICKER"));
+						break;
 
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      }     
-      
-    } catch (ClassNotFoundException e) {
-      e.printStackTrace();
-      //BugSenseHandler.sendException(e);
-    }
+					case AudioManager.RINGER_MODE_NORMAL:
+						edtInformation.visible(true);
+						edtInformation
+						.status(getString(R.string.current_profile)
+								+ " \u2014 " + (String) getName
+								.invoke(getActiveProfile
+										.invoke(o)));
+						edtInformation.expandedBody(getString(R.string.ringer_normal));
+						edtInformation.clickIntent(new Intent("android.intent.action.PROFILE_PICKER"));
+						break;
+					}
 
-        edtInformation.icon(R.drawable.ic_dashclock);
-        publishUpdate(edtInformation);
-        Log.d("ProfilicWidget", "Done");
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					e.printStackTrace();
+				}
 
-    }
+			} catch (NoSuchMethodException e) {
+				e.printStackTrace();
+			}     
 
-    /*
-     * @see com.google.android.apps.dashclock.api.DashClockExtension#onDestroy()
-     */
-    public void onDestroy() {
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			//BugSenseHandler.sendException(e);
+		}
 
-        super.onDestroy();
-        Log.d("ProfilicWidget", "Destroyed");
-        BugSenseHandler.closeSession(this);
+		edtInformation.icon(R.drawable.ic_dashclock);
+		publishUpdate(edtInformation);
+		Log.d("ProfilicWidget", "Done");
 
-    }
+	}
+
+	/*
+	 * @see com.google.android.apps.dashclock.api.DashClockExtension#onDestroy()
+	 */
+	public void onDestroy() {
+
+		super.onDestroy();
+		Log.d("ProfilicWidget", "Destroyed");
+		BugSenseHandler.closeSession(this);
+
+	}
 
 }
